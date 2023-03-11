935d757ef6dde548954d6428904fbb3e66adc0ff
sivareddysap48 <sivareddysap48@gmail.com>
2023-03-11 19:53:14 +0530

    changed war

--- src/main/java/net/guides/springboot2/springboot2jpacrudexample/tutorial/Application.java
+++ src/main/java/net/guides/springboot2/springboot2jpacrudexample/tutorial/Application.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -12,7 +14,16 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan(basePackages="net.guides.springboot2.springboot2jpacrudexample")
 @EntityScan("net.guides.springboot2.springboot2jpacrudexample")
 @EnableJpaRepositories("net.guides.springboot2.springboot2jpacrudexample")
-public class Application {
+public class Application  extends SpringBootServletInitializer {
+
+	 @Override
+	 
+	     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	 
+	         return application.sources(Application.class);
+	 
+	     }
+	 
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
